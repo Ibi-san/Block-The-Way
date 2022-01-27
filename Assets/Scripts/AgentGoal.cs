c114e7fcd941980e639a0f828f3b9f55f60aa18f
Ibi-san <89692342+Ibi-san@users.noreply.github.com>
2022-01-28 01:21:59 +0300

    version 0.1
    Reworked to isometric

--- Assets/Scripts/AgentGoal.cs
+++ Assets/Scripts/AgentGoal.cs
@@ -5,10 +5,16 @@ using UnityEngine.AI;
 
 public class AgentGoal : MonoBehaviour
 {
-    [SerializeField] private Transform Goal;
+    [SerializeField] private Transform _goal;
     void Start()
     {
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = Goal.position;
+        agent.destination = _goal.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Time.timeScale = 0;
+        Debug.Log("Loose!");
     }
 }
--- Assets/Scripts/DragObject.cs
+++ Assets/Scripts/DragObject.cs
@@ -2,27 +2,39 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
 
 public class DragObject : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float _sensetivity;
+    [SerializeField] private float _objectMaxSpeed = 200f;
 
-    private void Start()
+    private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
 
+    private void FixedUpdate()
+    {
+        if (_rigidbody.velocity.magnitude > _objectMaxSpeed)
+        {
+            _rigidbody.velocity = _rigidbody.velocity.normalized * _objectMaxSpeed;
+        }
+    }
     private void OnMouseDrag()
     {
         //При нажатии на предмет и его передвижении, сохраняем величину и направление движения мыши(пальца) в переменную и передаём эту переменную в двигающую силу
 
-        float distanceX = Input.GetAxis("Mouse X");  
+        float distanceX = Input.GetAxis("Mouse X") * Time.deltaTime;
+        //float _distanceX = Mathf.Clamp(distanceX, -1f, 1f) * Time.deltaTime;
+        Debug.Log(distanceX);
         if (distanceX != 0)
-            _rigidbody.AddForce(distanceX, 0, 0, ForceMode.VelocityChange); 
+            _rigidbody.AddForce(distanceX * _sensetivity, 0, distanceX * _sensetivity, ForceMode.VelocityChange); 
 
-        float distanceZ = Input.GetAxis("Mouse Y");
+        float distanceZ = Input.GetAxis("Mouse Y") * Time.deltaTime;
         if (distanceZ != 0)
-            _rigidbody.AddForce(0, 0, distanceZ, ForceMode.VelocityChange);
+            _rigidbody.AddForce(-distanceZ * _sensetivity, 0, distanceZ * _sensetivity, ForceMode.VelocityChange);
 
     }
 
